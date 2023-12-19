118cc75fc755ce1e35b62719f8b0ead744e4f480
Dabloo-9123 <dablook2000@gmail.com>
2023-12-19 15:10:38 +0530

    update

--- controller/productController.js
+++ controller/productController.js
@@ -4,19 +4,6 @@ const data=require('../data')
 const collectdata=(req,res)=>{
     res.send(data)
 }
-// const SearchProduct=(async (req,res)=>{
-//     console.log(req.body);
-//     let data= await data.find(
-//         {
-//             "$or":[
-//                 {"name":{$regex:req.body}},
-               
-//                 {"category":{$regex:req.body}}
-//             ]
-//         }
-//     )
-//     res.send(data)
-//     // console.log(data);
-// })
-module.exports={collectdata};
+
+module.exports=collectdata;
 
